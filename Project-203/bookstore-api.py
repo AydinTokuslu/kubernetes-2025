b1ef7f30573a929a5fa94fc99d69d97969390057
AydinTokuslu <aydintokuslu78@gmail.com>
2025-04-03 17:41:18 +0200

    Project-203

--- Project-203/bookstore-api.py
+++ Project-203/bookstore-api.py
@@ -6,7 +6,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure sqlite database
-app.config['MYSQL_DATABASE_HOST'] = 'database' # docker-compose.yml  içinde => bookstore-database:
+app.config['MYSQL_DATABASE_HOST'] = 'mysql' # docker-compose.yml  içinde => bookstore-database:
 app.config['MYSQL_DATABASE_USER'] = 'clarusway'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'bookstore_db'
